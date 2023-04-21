d7616e4919f3f43eb224eb65e20a275e86c59f4b
hernansaavedrag <hernan.saavedra@gmail.com>
2023-04-21 11:56:53 -0400

    pagina master y rutas

--- u2/test1/routes/web.php
+++ u2/test1/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EquiposController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +14,11 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 //Route::get('/', function () {
 //    return view('welcome');
 //});
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+Route::get('/login',[HomeController::class,'login'])->name('home.login');
 
-Route::get('/',[HomeController::class,'index']);
-Route::get('/login',[HomeController::class,'login']);
+Route::get('/equipos',[EquiposController::class,'index'])->name('equipos.index');
 
